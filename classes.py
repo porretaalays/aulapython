e60eb1503b35dbbe71fdaedbdb9a973c0e8c47e1
Lays Oliveira <lays.oliveirasouza22@gmail.com>
2019-07-19 11:55:29 -0300

    final

--- classes.py
+++ classes.py
@@ -1,3 +1,19 @@
+from random import randint
+
+NAIPES = ('Paus','Copas','Espadas','Ouros')
+RANKS = ('As',1)
+( 'Dois',2)
+( 'Tres',3)
+( 'Quatro',4)
+('Cinco',5)
+('Seis',6)
+('Sete',7)
+('Oito',8)
+('Nove',9)
+('Dez',10)
+('Valete',10)
+('Dama',10)
+('Rei',10)
 class Carta():
     def __init__(self, naipe, rank, valor):
         self.naipe = naipe
@@ -10,6 +26,10 @@ class Carta():
 class Baralho():
     def __init__(self):
         self.cartas = []
+        for naipe in NAIPES:
+            for rank in RANKS:
+                carta = Carta(naipe, rank[0], rank[1])
+                self.adicionar_cartas(carta)
 
     def __str__(self):
         if len(self.cartas) == 0:
@@ -28,6 +48,18 @@ class Baralho():
 
     def remover_carta(self):
        return self.cartas.pop(0)
+
+    def sortear(self):
+     numero_sorteado = randint(0,len(self.cartas) -1 )
+     return self.cartas.pop(numero_sorteado)
+# class Jogo21():
+#     def __init__():
+#         baralho = Baralho()
+#         carta1 = baralho.sortear()
+#         carta2 = baralho.sortear()
+#         self.pontuacao = carta1.pontuacao + carta2.pontuacao
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from classes import Carta, Baralho
 
-
-carta1 = Carta('Paus','As',1)
-carta2 = Carta('Copas','Rei',10)
-carta3 = Carta('Espadas', 'Cinco',5)
 baralho = Baralho()
 
-baralho.adicionar_cartas(carta1)
-baralho.adicionar_cartas(carta2)
-baralho.adicionar_cartas(carta3)
-baralho.remover_carta()
+print(baralho.sortear())
+print(baralho.sortear())
+print(baralho.sortear())
 
-print(baralho.remover_carta())
+
+
+
+
+
+
+
+# carta1 = Carta('Paus','As',1)
+# carta2 = Carta('Copas','Rei',10)
+# carta3 = Carta('Espadas', 'Cinco',5)
+# baralho = Baralho()
+# print(baralho)
+
+
+# # baralho.adicionar_cartas(carta1)
+# # baralho.adicionar_cartas(carta2)
+# # baralho.adicionar_cartas(carta3)
+# # baralho.remover_carta()
+
+# # print(baralho.remover_carta())
+
 
